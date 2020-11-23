d96a21a94e95a22bc9a2596ac4e02919a0eee348
Patrick Kersey <pskersey@yahoo.com>
2020-11-23 06:21:24 -0600

    Mana Bar
    Mana Bar has been implemented. Although since the arcane explosion is still not working, mana bar is just Debug Logs for now.

---
+++ Assets/Scripts/ManaBar_cs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar_cs : MonoBehaviour
+{
+    public Slider _manaSlider;
+
+    public void SetMaxMana(int mana)
+    {
+        _manaSlider.maxValue = mana;
+        _manaSlider.value = mana;
+    }
+
+    public void SetMana(int mana)
+    {
+        _manaSlider.value = mana;
+    }
+}
+
