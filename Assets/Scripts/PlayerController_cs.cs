c71ad913e03d38cfb90e00ec4fa11a32f771b2db
Patrick Kersey <pskersey@yahoo.com>
2020-11-23 06:21:24 -0600

    Mana Bar
    Mana Bar has been implemented. Although since the arcane explosion is still not working, mana bar is just just Debug Logs for now.

--- Assets/Scripts/PlayerController_cs.cs
+++ Assets/Scripts/PlayerController_cs.cs
@@ -6,13 +6,46 @@ public class PlayerController_cs : MonoBehaviour
 {
     public CharacterController controller;
 
+    public ManaBar_cs manaBar;
+
     public float turnspeed = 100f;
     public float movespeed = 6f;
+
+    public int maxMana = 100;
+    public int currentMana;
+    public float manaRegen = 3;
+
+    private void Start()
+    {
+        currentMana = maxMana;
+        manaBar.SetMaxMana(maxMana);
+    }
+
     void Update()
     {
        
         transform.Rotate(Vector3.up * turnspeed * Input.GetAxis("Horizontal") * Time.deltaTime);
         transform.Translate(0f, 0f, movespeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && currentMana >= 20)
+        {
+            CastArcaneExplosion();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && currentMana <20)
+        {
+            Debug.Log("Not Enough Mana!");
+        }
+
+
+        
+    }
+
+    public void CastArcaneExplosion()
+    {
+        currentMana -= 20;
+        manaBar.SetMana(currentMana);
+        Debug.Log("Casted Arcane Explosion!");
 
     }
 }
